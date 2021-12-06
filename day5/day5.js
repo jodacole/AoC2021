5e1ed5128c3b538997f880f3429f1fd772945e86
Josh Cole <jdc122897@yahoo.com>
2021-12-05 21:41:58 -0500

    part 1 complete

--- day5/day5.js
+++ day5/day5.js
@@ -1,19 +1,41 @@
 const fs = require("fs");
+const _ = require("lodash");
 
 fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
     console.log(err);
     return;
   } else {
-    console.log(processData(data));
+    console.log(part1(data));
   }
 });
 
 function processData(data) {
-  let lines = data
-    .split("\n")
-    .map((l) =>
-      l.split(" -> ").map((str) => parseInt(str.replace(",", ""), 10))
-    );
-  return lines;
+  return data.split("\n").map((line) =>
+    line
+      .split(" -> ")
+      .map((coord) => coord.split(",").map((str) => parseInt(str, 10)))
+      .map((coord) => {
+        return { x: coord[0], y: coord[1] };
+      })
+  );
+}
+
+function part1(data) {
+  lines = processData(data).filter(
+    (line) => line[0].x === line[1].x || line[0].y === line[1].y
+  );
+  let squaresToNumVents = {};
+  for (let line of lines) {
+    currSquare = JSON.parse(JSON.stringify(line[0]));
+    while (!_.isEqual(currSquare, line[1])) {
+      let key = "x" + currSquare.x + "y" + currSquare.y;
+      squaresToNumVents[key] = squaresToNumVents[key] + 1 || 1;
+      currSquare.x += Math.sign(line[1].x - currSquare.x);
+      currSquare.y += Math.sign(line[1].y - currSquare.y);
+    }
+    let key = "x" + line[1].x + "y" + line[1].y;
+    squaresToNumVents[key] = squaresToNumVents[key] + 1 || 1;
+  }
+  return Object.values(squaresToNumVents).filter((n) => n >= 2).length;
 }
