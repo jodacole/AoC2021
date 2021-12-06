b7d03f63452cfa002e3dd70efae046239c1b2f71
Josh Cole <jdc122897@yahoo.com>
2021-12-06 01:39:35 -0500

    finished day6

--- day6/day6.js
+++ day6/day6.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
+const _ = require("lodash");
 
 fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
     console.log(err);
     return;
   } else {
+    console.log(part1("3,4,3,1,2"));
     console.log(part1(data));
+    console.log(part2("3,4,3,1,2"));
+    console.log(part2(data));
   }
 });
 
@@ -14,14 +18,33 @@ function part1(data) {
 
   const days = 80;
   for (let i = 0; i < days; i++) {
-    console.log(fishes[0]);
-    fishes = fishes.map((f) => f - 1);
-    let newFishes = fishes.reduce((total, fish) => {
-      return fish < 0 ? total + 1 : total;
-    });
-    fishes = fishes.map((f) => (f < 0 ? 6 : f));
-    fishes = fishes.concat(new Array(newFishes).fill(7));
+    fishes = _.map(fishes, (f) => f - 1);
+    let newFishes = _.reduce(
+      fishes,
+      (total, fish) => {
+        return fish < 0 ? total + 1 : total;
+      },
+      0
+    );
+    fishes = _.map(fishes, (f) => (f < 0 ? 6 : f));
+    fishes = fishes.concat(new Array(newFishes).fill(8));
   }
 
   return fishes.length;
+}
+
+function part2(data) {
+  let fishes = data.split(",").map((f) => parseInt(f, 10));
+  fishTimers = new Array(9).fill(0);
+  fishes.map((f) => (fishTimers[f] += 1));
+  const days = 256;
+  for (let d = 0; d < days; d++) {
+    const numZeros = fishTimers[0];
+    for (let i = 0; i < 8; i++) {
+      fishTimers[i] = fishTimers[i + 1];
+    }
+    fishTimers[6] += numZeros;
+    fishTimers[8] = numZeros;
+  }
+  return _.sum(fishTimers);
 }
