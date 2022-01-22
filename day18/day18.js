d7c679ab349a0e1b26ad1651bf177dff5642ae7a
Josh Cole <jdc122897@yahoo.com>
2022-01-21 20:56:58 -0500

    s

--- day18/day18.js
+++ day18/day18.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const _ = require("lodash");
-const { parse } = require("querystring");
 
 fs.readFile("input.txt", "utf8", (err, data) => {
   if (err) {
@@ -18,10 +17,10 @@ function processInput(data) {
     let currPath = [];
     for (let char of num) {
       if (char === "[") {
-        currPath.push("left");
+        currPath.push("l");
       } else if (char === ",") {
         currPath.pop();
-        currPath.push("right");
+        currPath.push("r");
       } else if (char === "]") {
         currPath.pop();
       } else {
@@ -34,21 +33,87 @@ function processInput(data) {
 
 function part1(data) {
   const snailNums = processInput(data);
-  console.log(_.get(snailNums[0], ["left"]));
-  return JSON.stringify(snailNums[0], null, 2);
+  console.log(JSON.stringify(snailNums[0], null, 2));
+  // return snailNumToString(snailNums[0]);
+  return snailNumToString(reduce(snailNums[0]));
 }
 
-function reduce(snailNum) {}
-
-function explode(snailNum) {
-  const agenda = [["left"], ["right"]];
+function reduce(snailNum) {
+  console.log(snailNumToString(snailNum));
+  const agenda = [["r"], ["l"]];
+  let pathToSplittable = false;
   while (!_.isEmpty(agenda)) {
-    currPath = agenda.unshift();
-    if (currPath.length > 4) {
+    const currPath = agenda.pop();
+    const valueAtCurrPath = _.get(snailNum, currPath);
+    if (valueAtCurrPath && currPath.length === 5) {
+      console.log("exploding...");
+      explode(snailNum, currPath);
+      return reduce(snailNum);
+    } else if (!Number.isInteger(valueAtCurrPath)) {
+      agenda.push(currPath.concat("r"));
+      agenda.push(currPath.concat("l"));
+    } else if (!pathToSplittable && valueAtCurrPath >= 10) {
+      pathToSplittable = currPath;
     }
   }
+  if (pathToSplittable) {
+    console.log("splitting...");
+    split(snailNum, pathToSplittable);
+    return reduce(snailNum);
+  }
+  return snailNum;
 }
 
-function isNumber(obj) {
-  return Number.isInteger(obj);
+function split(snailNum, pathToSplittable) {
+  const valAtPath = _.get(snailNum, pathToSplittable);
+  _.set(snailNum, pathToSplittable, {
+    l: Math.floor(valAtPath / 2),
+    r: Math.ceil(valAtPath / 2),
+  });
+}
+
+function explode(snailNum, currPath) {
+  const leftDebris = _.get(snailNum, currPath.slice(0, -1).concat("l"));
+  const rightDebris = _.get(snailNum, currPath.slice(0, -1).concat("r"));
+  let tempPath = currPath.slice(0, -2);
+  let leftDebrisAdded = false;
+  let rightDebrisAdded = false;
+  while (tempPath) {
+    currLeftVal = _.get(snailNum, tempPath.concat("l"));
+    if (!leftDebrisAdded && Number.isInteger(currLeftVal)) {
+      _.set(snailNum, tempPath.concat("l"), currLeftVal + leftDebris);
+      leftDebrisAdded = true;
+    }
+
+    currRightVal = _.get(snailNum, tempPath.concat("r"));
+    if (!rightDebrisAdded && Number.isInteger(currRightVal)) {
+      _.set(snailNum, tempPath.concat("r"), currRightVal + rightDebris);
+      rightDebrisAdded = true;
+    }
+    if (_.isEmpty(tempPath)) {
+      tempPath = false;
+    } else {
+      tempPath.pop();
+    }
+  }
+  _.set(snailNum, currPath.slice(0, -1), 0);
+}
+
+function snailNumToString(snailNum) {
+  if (!snailNum) {
+    return snailNum;
+  }
+  result = "[";
+  if (Number.isInteger(snailNum.l)) {
+    result += snailNum.l;
+  } else {
+    result += snailNumToString(snailNum.l);
+  }
+  result += ",";
+  if (Number.isInteger(snailNum.r)) {
+    result += snailNum.r;
+  } else {
+    result += snailNumToString(snailNum.r);
+  }
+  return result + "]";
 }
